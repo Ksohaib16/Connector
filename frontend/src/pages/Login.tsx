633c85ad8f51132be85749b03b67e1f9dd393ddf
sohaib k <1234sohaibk@gmail.com>
2025-02-04 01:11:41 +0530

    login button fix

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,133 +1,127 @@
-import axios from "axios";
-import { Mail, Lock } from "lucide-react";
-import { AuthHeader } from "../components/AuthHeader";
-import { AuthForm } from "../components/AuthForm";
-import { AuthWrapper } from "../components/wrapper/AuthWrapper";
-import { useState } from "react";
-import { validateLoginForm } from "../utility/validateForm";
-import { app } from "../config/Firebase";
-import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
-import { useDispatch } from "react-redux";
-import { setUser } from "../redux/userSlice";
-import { Link, useNavigate } from "react-router-dom";
-import { MainWrapper } from "../components/wrapper/MainWrapper";
-import { ContentWrapper } from "../components/wrapper/ContentWrapper";
-import { config } from "../config/api.config";
+import axios from 'axios';
+import { Mail, Lock } from 'lucide-react';
+import { AuthHeader } from '../components/AuthHeader';
+import { AuthForm } from '../components/AuthForm';
+import { AuthWrapper } from '../components/wrapper/AuthWrapper';
+import { useState } from 'react';
+import { validateLoginForm } from '../utility/validateForm';
+import { app } from '../config/Firebase';
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
+import { useDispatch } from 'react-redux';
+import { setUser } from '../redux/userSlice';
+import { Link, useNavigate } from 'react-router-dom';
+import { MainWrapper } from '../components/wrapper/MainWrapper';
+import { ContentWrapper } from '../components/wrapper/ContentWrapper';
+import { config } from '../config/api.config';
 
 const auth = getAuth(app);
 
 const authDetails = {
-  heading: "Connector",
-  subHeading: "Please confirm you email and password",
+    heading: 'Connector',
+    subHeading: 'Please confirm you email and password',
 };
 
 const inputDetails = [
-  { type: "text", placeholder: "Email", name: "email", icon: Mail },
-  { type: "password", placeholder: "Password", name: "password", icon: Lock },
+    { type: 'text', placeholder: 'Email', name: 'email', icon: Mail },
+    { type: 'password', placeholder: 'Password', name: 'password', icon: Lock },
 ];
 
 interface FormErrors {
-  [key: string]: string;
+    [key: string]: string;
 }
 
 //function
 export const Login = () => {
-  const [formData, setFormData] = useState({
-    email: "",
-    password: "",
-  });
-
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
-  const [errors, setErrors] = useState<FormErrors>({});
-  const [isLoggingIn, setIsLoggingIn] = useState(false);
-
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setFormData((currData) => {
-      return { ...currData, [e.target.name]: e.target.value };
+    const [formData, setFormData] = useState({
+        email: '',
+        password: '',
     });
-    setErrors({});
-  };
 
-  const loginUser = async (email: string) => {
-    const user = auth.currentUser;
-    const token = await user?.getIdToken();
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const [errors, setErrors] = useState<FormErrors>({});
+    const [isLoggingIn, setIsLoggingIn] = useState(false);
 
-    const response = await axios.post(
-      `${config.API_URL}/api/v1/auth/login`,      {
-        email,
-      },
-      {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      }
-    );
-    const userData = response.data;
-    setFormData({
-      email: "",
-      password: "",
-    });
-    return userData;
-  };
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setFormData((currData) => {
+            return { ...currData, [e.target.name]: e.target.value };
+        });
+        setErrors({});
+    };
 
-  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    const newErrors = validateLoginForm(formData);
-    console.log("newError", newErrors);
-    setErrors(newErrors);
-    if (Object.keys(newErrors).length === 0) {
-      try {
-        setIsLoggingIn(true);
-        await signInWithEmailAndPassword(
-          auth,
-          formData.email,
-          formData.password
+    const loginUser = async (email: string) => {
+        const user = auth.currentUser;
+        const token = await user?.getIdToken();
+
+        const response = await axios.post(
+            `${config.API_URL}/api/v1/auth/login`,
+            {
+                email,
+            },
+            {
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                },
+            },
         );
+        const userData = response.data;
+        setFormData({
+            email: '',
+            password: '',
+        });
+        return userData;
+    };
 
-        const userData = await loginUser(formData.email);
-        if (!userData) {
-          return;
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        const newErrors = validateLoginForm(formData);
+        console.log('newError', newErrors);
+        setErrors(newErrors);
+        if (Object.keys(newErrors).length === 0) {
+            try {
+                setIsLoggingIn(true);
+                await signInWithEmailAndPassword(auth, formData.email, formData.password);
+
+                const userData = await loginUser(formData.email);
+                if (!userData) {
+                    return;
+                }
+                console.log(userData, userData.status);
+                dispatch(setUser(userData.data.user));
+                navigate('/home');
+            } catch {
+                setErrors((prev) => ({
+                    ...prev,
+                    password: 'Invalid email or password',
+                }));
+            } finally {
+                setIsLoggingIn(false);
+            }
         }
-        console.log(userData, userData.status);
-        dispatch(setUser(userData.data.user));
-        navigate("/home");
-      } catch {
-        setErrors((prev) => ({
-          ...prev,
-          password: "Invalid email or password",
-        }));
-      } finally {
-        setIsLoggingIn(false);
-      }
-    }
-  };
+    };
 
-  return (
-    <MainWrapper>
-      <ContentWrapper>
-        <AuthWrapper>
-          <AuthHeader
-            heading={authDetails.heading}
-            subHeading={authDetails.subHeading}
-          />
-          <AuthForm
-            errors={errors}
-            handleSubmit={handleSubmit}
-            handleChange={handleChange}
-            values={formData}
-            inputDetails={inputDetails}
-            button={"SIGN UP"}
-            isLoggin={isLoggingIn}
-          />
-          <div className="text-[#AAAAAA]">
-            Don't have an account? &nbsp;{" "}
-            <b className="text-[#3874c9]">
-              <Link to="/signup">Sign up</Link>
-            </b>
-          </div>
-        </AuthWrapper>
-      </ContentWrapper>
-    </MainWrapper>
-  );
+    return (
+        <MainWrapper>
+            <ContentWrapper>
+                <AuthWrapper>
+                    <AuthHeader heading={authDetails.heading} subHeading={authDetails.subHeading} />
+                    <AuthForm
+                        errors={errors}
+                        handleSubmit={handleSubmit}
+                        handleChange={handleChange}
+                        values={formData}
+                        inputDetails={inputDetails}
+                        button={'LOG IN'}
+                        isLoggin={isLoggingIn}
+                    />
+                    <div className="text-[#AAAAAA]">
+                        Don't have an account? &nbsp;{' '}
+                        <b className="text-[#3874c9]">
+                            <Link to="/signup">Sign up</Link>
+                        </b>
+                    </div>
+                </AuthWrapper>
+            </ContentWrapper>
+        </MainWrapper>
+    );
 };
